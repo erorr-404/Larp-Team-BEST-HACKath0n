f8724104a9e66eaa61c4fd3639cd275010c8ef58
Maksym Holub <maxymholub@gmail.com>
2026-04-03 11:36:14 +0300

    Structurized, created Metrics Plan

--- Code/ParserTest/ParserTest/Program.cs
+++ Code/ParserTest/ParserTest/Program.cs
@@ -4,8 +4,8 @@ public class App
 {
     public static void Main()
     {
-        string fileName = "00000001.BIN";
-        string outputCsv = "log_export.csv";
+        string fileName = "tmp/00000001.BIN";
+        string outputCsv = "tmp/log_export.csv";
 
         using DFLogBuffer logdata = new(fileName);
         DFLog dfLog = logdata.dflog;
@@ -14,7 +14,7 @@ public class App
 
         Console.WriteLine("--- First 10 records (parsed) ---");
         int shown = 0;
-        foreach (var item in logdata.GetEnumeratorTypeAll())
+        foreach (var item in logdata.GetEnumeratorType("GPS[0].Alt"))
         {
             Console.WriteLine(string.Join(",", item.items));
             shown++;
@@ -25,9 +25,9 @@ public class App
         // Example: export only GPS records to CSV
         using (var writer = new StreamWriter(outputCsv))
         {
-            writer.WriteLine("msgtype,time,raw");
+            // writer.WriteLine("msgtype,time,raw");
 
-            foreach (var item in logdata.GetEnumeratorType("GPS"))
+            foreach (var item in logdata.GetEnumeratorTypeAll())
             {
                 var raw = string.Join(";", item.items).Replace("\"", "\"\"");
                 writer.WriteLine($"{item.msgtype},{item.time:O},\"{raw}\"");
