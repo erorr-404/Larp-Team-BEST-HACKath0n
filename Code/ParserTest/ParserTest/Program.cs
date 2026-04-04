e19fcd5917fa45639dc2bc8fe5fc311b6b54b2a6
Maksym Holub <maxymholub@gmail.com>
2026-04-04 16:13:26 +0300

    Changed Exe to Dll in building

--- Code/ParserTest/ParserTest/Program.cs
+++ Code/ParserTest/ParserTest/Program.cs
@@ -1,42 +1,53 @@
 ﻿using MissionPlanner.Utilities;
 
-public class App
+public class KinematicDataProcessor
 {
-    public static void Main()
+    // public static void Main()
+    // {
+
+    //     // ❗ Як запускати: dotnet run > tmp/struct.log
+    //     // Файл можна взяти інший, головгне не знищити корисні дані
+
+    //     // string fileName = "/home/maxym/Документи/BestHackathon/Code/ParserTest/ParserTest/tmp/00000001.BIN";
+
+    //     // BinaryParser parser = new(fileName);
+    //     // parser.Parse();
+
+    //     // KinematicCalculator calculator = new();
+    //     // calculator.CalculateKinematicPointsFromRecords(parser.GpsRecords, parser.ImuRecords, parser.BaroRecords);
+
+    //     // foreach (KinematicPoint point in calculator.kinematicPoints)
+    //     // {
+    //     //     Console.WriteLine(point.ToString());
+    //     // }
+
+    //     // Console.WriteLine("--- GPS Records ---");
+    //     // foreach (var item in parser.GpsRecords)
+    //     // {
+    //     //     Console.WriteLine(item.ToString());
+    //     // }
+
+    //     // Console.WriteLine("--- IMU Records ---");
+    //     // foreach (var item in parser.ImuRecords)
+    //     // {
+    //     //     Console.WriteLine(item.ToString());
+    //     // }
+
+    //     // Console.WriteLine("--- BARO Records ---");
+    //     // foreach (var item in parser.BaroRecords)
+    //     // {
+    //     //     Console.WriteLine(item.ToString());
+    //     // }
+    // }
+
+    public static KinematicPoint[] GetKinematicPointsFromBinaryFile(string fileName)
     {
-
-        // ❗ Як запускати: dotnet run > tmp/struct.log
-        // Файл можна взяти інший, головгне не знищити корисні дані
-
-        string fileName = "/home/maxym/Документи/BestHackathon/Code/ParserTest/ParserTest/tmp/00000001.BIN";
-
         BinaryParser parser = new(fileName);
         parser.Parse();
 
         KinematicCalculator calculator = new();
         calculator.CalculateKinematicPointsFromRecords(parser.GpsRecords, parser.ImuRecords, parser.BaroRecords);
 
-        foreach (KinematicPoint point in calculator.kinematicPoints)
-        {
-            Console.WriteLine(point.ToString());
-        }
-
-        // Console.WriteLine("--- GPS Records ---");
-        // foreach (var item in parser.GpsRecords)
-        // {
-        //     Console.WriteLine(item.ToString());
-        // }
-
-        // Console.WriteLine("--- IMU Records ---");
-        // foreach (var item in parser.ImuRecords)
-        // {
-        //     Console.WriteLine(item.ToString());
-        // }
-
-        // Console.WriteLine("--- BARO Records ---");
-        // foreach (var item in parser.BaroRecords)
-        // {
-        //     Console.WriteLine(item.ToString());
-        // }
+        return calculator.kinematicPoints;
     }
 }
